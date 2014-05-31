bf7e43c647b4c3f026365281caca943b43b93b60
LimeCoin <handsomeman_rw@hotmail.com>
2014-05-31 14:05:02 -0500

    remove 0xA9 char

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -21,8 +21,8 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
     // define text to place
     QString titleText       = QString(QApplication::applicationName()).replace(QString("-testnet"), QString(""), Qt::CaseSensitive); // cut of testnet, place it as single object further down
     QString versionText     = QString("").arg(QString::fromStdString(FormatFullVersion()));
-    QString copyrightText1   = QChar(0xA9)+QString("").arg(COPYRIGHT_YEAR) + QString(tr(""));
-    QString copyrightText2   = QChar(0xA9)+QString("").arg(COPYRIGHT_YEAR) + QString(tr(""));
+    QString copyrightText1   = QChar()+QString("").arg(COPYRIGHT_YEAR) + QString(tr(""));
+    QString copyrightText2   = QChar()+QString("").arg(COPYRIGHT_YEAR) + QString(tr(""));
 
     QString font            = "Arial";
 
