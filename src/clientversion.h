ac6affd45aa75c5054d12edf3043be3dcf81f125
Christian Knöpke <apfelbaum@email.de>
2015-02-18 19:38:50 +0100

    Update clientversion.h

--- src/clientversion.h
+++ src/clientversion.h
@@ -12,11 +12,12 @@
 #define CLIENT_VERSION_BUILD       3
 
 // Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE  true
+// Vorher true
+#define CLIENT_VERSION_IS_RELEASE  false
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2014
+#define COPYRIGHT_YEAR 2015
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
